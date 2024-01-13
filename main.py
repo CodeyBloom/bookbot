6f97c1acb65ac754d2b42e6666c97a1bc4ed8fc6
codeybloom <codey.bloom@protonmail.com>
2024-01-14 09:46:06 +1100

    Change word counr print to include full sentence

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
+    '''Making a script to print the file contents'''
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
-
+    print(file_contents)
+        
+    '''Making a script to count the words'''
+    words = file_contents.split()
+    count = 0
+    for word in words:
+        count += 1
+    print(f"This document had {count} words")
 
 main()
 
